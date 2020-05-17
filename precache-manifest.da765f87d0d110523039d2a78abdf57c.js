4962b10e2def72139853a8bd44fe783443ac6924
Jieun Lee <lje7128@gmail.com>
2020-05-17 18:58:39 +0900

    Updates

---
+++ precache-manifest.da765f87d0d110523039d2a78abdf57c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66114c082be523bb6f284b68a9227c6",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "934b5a6da12dc50216a2",
+    "url": "/movie_app_2020/static/css/main.7126812c.chunk.css"
+  },
+  {
+    "revision": "261fc6083f1d5d248b8b",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934b5a6da12dc50216a2",
+    "url": "/movie_app_2020/static/js/main.bea922a8.chunk.js"
+  },
+  {
+    "revision": "9927c9d5c16bc5ac0d63",
+    "url": "/movie_app_2020/static/js/runtime-main.72517e8e.js"
+  }
+]);
