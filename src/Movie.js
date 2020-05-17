c80838c8e10aed6f788cd0b3c3422f70ef879fcd
Jieun Lee <lje7128@gmail.com>
2020-05-17 18:49:24 +0900

    #4.5 Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -9,12 +9,12 @@ return (
         <div className="movies__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
+            <ul className="movie__genres">
                 {genres.map((genre, index) => (
                     <li key={index} className="genres__genre">{genre}</li>
                 ))}
             </ul>
-            <p className="movie__summary">{summary}</p>
+            <p className="movie__summary">{summary.slice(0, 140)}</p>
             
         </div>
     </div>
